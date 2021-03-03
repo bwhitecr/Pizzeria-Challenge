c17c655db4ea897b5cfe85ed55d2e58f6c12366d
Brad Whitecross <brad.whitecross@independentsolutions.com.au>
2021-03-03 21:51:03 +1000

    Added React frontend

---
+++ client/pizzeria-frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>Welcome to the LOR Pizzeria</h1>
+        </header>
+    );
+}
+
+export default Header;
